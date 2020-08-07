26e7eae0508b0492334316fba26ce65f220a93f9
DJ Stewart <deshastewart1992@gmail.com>
2020-08-07 00:41:20 -0700

    not finished yet

---
+++ videoOne.js
@@ -0,0 +1,68 @@
+let p = new Promise((resolve, reject) => {
+    let a = 1 + 2
+    if (a == 2) {
+      resolve('Success')
+    } else {
+      reject('Failed')
+    }
+  })
+  
+  
+  p.then((message) => {
+    console.log('This is in the then ' + message)
+  }).catch((message)  => {
+    console.log('This is in the catch ' + message)
+  })
+  
+  //next one
+  const userLeft = false
+  const userWatchingCatMeme = true 
+  
+  function watchTutorialPromise() {
+    return new Promise((resolve, reject) => {
+      if (userLeft) {
+        reject({
+          name: 'User Left', 
+          message: ':('
+        })
+      } else if (userWatchingCatMeme) {
+        reject({ 
+        name: 'User Watching Cat Meme', 
+        message: 'webDevSimplified < Cat'
+        })
+      } else {
+        resolve('Thumbs up and Subscribe')
+      }
+    })
+  }
+  
+  
+  watchTutorialPromise().then((message) => {
+    console.log('Success: ' + message)
+  })then((message) => {
+    console.log('Success: ' + message)
+  }).catch((error) => {
+    console.log(error.name + ' ' + error.message)
+  })
+  
+  // next one 
+  const recordVideoOne = new Promise((resolve, reject) => {
+    resolve('Video 1 Recorded')
+  })
+  
+  const recordVideoTwo = new Promise((resolve, reject) => {
+    resolve('Video 2 Recorded')
+  })
+  
+  const recordVideoThree = new Promise((resolve, reject) => {
+    resolve('Video 3 Recorded')
+  })
+  
+  Promise.race([
+    recordVideoOne,
+    recordVideoTwo,
+    recordVideoThree
+  ]).then((message) => {
+    console.log(message)
+  })
+  
